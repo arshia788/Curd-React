c6c71e76e977bb91ead1e7bec343d2bc8c9a268b
Amirali Amanipour <arshia.ala80@gmail.com>
2023-06-29 21:25:08 +0330

    working on the backend and checking for reciving the data

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,8 @@
 import express from 'express';
-
-// ba in miay vasl mikoni 
 import mysql from 'mysql';
 
 const app = express();
 
-// * createConnection ba in miay vasal mishi 
 const db= mysql.createConnection({
     host:"localhost",
     user:"root",
@@ -13,22 +10,31 @@ const db= mysql.createConnection({
     database:"crud-project"
 })
 
-// inja mikhay on response ro begiri
-// aval mishe on rishe ma
 
 app.get('/', (req,res)=>{
     res.json("Hii there")
 })
 
-// local host ma shod 3003 
+
+// rafti on data ro begiri
+app.get('/cars', (req, res)=>{
+
+    // gofti har chizi ke dakhel table cars hast ro neshon bedeh
+    const q = "SELECT * FROM CARS"
+
+    // inja rafti ye query zadi on db ro v on q ro dadi behesh
+    // aval err va dovom data ro gerefti
+    db.query(q, (err, data)=>{
+        
+        // gofti agar err bod on ro neshon bedeh agar na biyad data ro neshon bedeh 
+        if(err) return res.json(err)
+
+        return res.json(data)
+    } )
+})
+
 app.listen(3003, ()=>{
     console.log('hello there!');
 
 })
-
-
-
-
-
-
 
